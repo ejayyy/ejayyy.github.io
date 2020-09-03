cfbfc1bbfb2603204e59c07714656238b9555f14
ejayyy <ejay0811@gmail.com>
2020-09-03 15:51:33 -0700

    [add] tutorial
    nested layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import React from "react"
 import { Link } from "gatsby"
 import Header from "../components/header"
+import Layout from "../components/layout"
 
 export default function Home() {
   return (
-    <div style={{ color: `purple` }}>
-      <Link to="/contact/">Contact</Link>
+    <Layout>
       <Header headerText="Hi WORLD" />
       <p>How sleepy</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
-    </div>)
+    </Layout>
+  )
 }
