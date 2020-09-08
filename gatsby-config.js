b708ab1c2d4e6eba8a9c42efce3207b01f9a0a07
ejayyy <ejay0811@gmail.com>
2020-09-07 20:01:50 -0700

    [add] tutorial 8

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,9 +6,27 @@
 
 module.exports = {
   siteMetadata: {
-    title: `EJ's brand new blog`,
+    title: `EJ blog`,
+    description: `Personal journal`,
+    author: `EJ`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,12 +1,14 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 export default function BlogPost({ data }) {
     const post = data.markdownRemark
 
     return (
         <Layout>
+            <SEO title={post.frontmatter.title} description={post.excerpt} />
             <div>
                 <h1>{post.frontmatter.title}</h1>
                 <div dangerouslySetInnerHTML={{ __html: post.html }} />
@@ -22,5 +24,6 @@ query($slug: String!){
         frontmatter{
             title
         }
+        excerpt
     }
 }`
