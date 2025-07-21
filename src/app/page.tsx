c2d95cd28743f866bb14f6783549bb3c2aa5f610
ejayyy <ejay0811@gmail.com>
2025-07-21 16:42:35 +0900

    Add title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,26 +2,30 @@ import { getPosts } from "./actions";
 import LaptopCatLight from "/public/laptop-cat-light.svg";
 import LaptopCatNight from "/public/laptop-cat-night.svg";
 import InfinitePostList from "../components/InfinitePostList";
+import Header from "@/components/Header";
 
 export default async function Home() {
   const { posts: initialPosts, hasMore: initialHasMore } = await getPosts(1, 5);
 
   return (
-    <div className="space-y-6">
-      <div className="sm:w-90 sm:mr-3 sm:float-left sm:mb-0">
-        <LaptopCatLight
-          className="w-full h-full fill-slate-600 dark:hidden"
-          aria-label="Laptop Cat Logo"
-        />
-        <LaptopCatNight
-          className="w-full h-full fill-rose-200 hidden dark:block"
-          aria-label="Laptop Cat Logo"
+    <>
+      <Header />
+      <div className="space-y-6">
+        <div className="sm:w-90 sm:mr-3 sm:float-left sm:mb-0">
+          <LaptopCatLight
+            className="w-full h-full fill-slate-600 dark:hidden"
+            aria-label="Laptop Cat Logo"
+          />
+          <LaptopCatNight
+            className="w-full h-full fill-rose-200 hidden dark:block"
+            aria-label="Laptop Cat Logo"
+          />
+        </div>
+        <InfinitePostList
+          initialPosts={initialPosts}
+          initialHasMore={initialHasMore}
         />
       </div>
-      <InfinitePostList 
-        initialPosts={initialPosts} 
-        initialHasMore={initialHasMore}
-      />
-    </div>
+    </>
   );
 }
