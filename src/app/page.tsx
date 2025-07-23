978d67a7ae1221e54d25576a07b4c3bdc16ed1bb
ejayyy <ejay0811@gmail.com>
2025-07-23 09:07:08 +0900

    Update import style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 import { getAllPosts } from "@/lib/posts";
 import { getPosts } from "@/lib/client-utils";
-import LaptopCatLight from "../../public/laptop-cat-light.svg";
-import LaptopCatNight from "../../public/laptop-cat-night.svg";
 import InfinitePostList from "@/components/InfinitePostList";
 import Header from "@/components/Header";
+import Image from "next/image";
 
 export default async function Home() {
   const allPosts = getAllPosts();
@@ -18,13 +17,21 @@ export default async function Home() {
       <Header />
       <div className="space-y-6">
         <div className="sm:w-90 sm:mr-3 sm:float-left sm:mb-0">
-          <LaptopCatLight
+          <Image
             className="w-full h-full fill-slate-600 dark:hidden"
             aria-label="Laptop Cat Logo"
+            src={"/laptop-cat-light.svg"}
+            alt="Laptop Cat Logo"
+            width={300}
+            height={115}
           />
-          <LaptopCatNight
+          <Image
             className="w-full h-full fill-rose-200 hidden dark:block"
             aria-label="Laptop Cat Logo"
+            src={"/laptop-cat-night.svg"}
+            alt="Laptop Cat Logo"
+            width={300}
+            height={115}
           />
         </div>
         <InfinitePostList
