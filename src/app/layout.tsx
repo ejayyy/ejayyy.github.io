1fc614d16e4b1cda1b67a8f360038c4138ca7826
ejayyy <ejay0811@gmail.com>
2025-08-05 15:43:17 +0900

    Update font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,8 @@
 import type { Metadata } from "next";
-import { IBM_Plex_Sans_KR, Orbit, Source_Code_Pro } from "next/font/google";
+import { Orbit, Source_Code_Pro } from "next/font/google";
 import Footer from "@/components/Footer";
 import { baseKeywords } from "@/lib/metadata-utils";
 import "./globals.css";
-
-const ibmPlexSansKR = IBM_Plex_Sans_KR({
-  variable: "--font-ibm-plex-sans-kr",
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700"],
-  display: "swap",
-});
 
 const sourceCodePro = Source_Code_Pro({
   variable: "--font-source-code-pro",
@@ -66,9 +59,32 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${ibmPlexSansKR.variable} ${sourceCodePro.variable} ${orbit.variable}`}
-      >
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Asta+Sans:wght@400;500;600&family=Cascadia+Code:wght@400;600&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Asta+Sans:wght@100;200;300;700&family=Cascadia+Code:wght@200;300;500;700&display=swap"
+          rel="stylesheet"
+          media="print"
+          // @ts-ignore
+          onLoad="this.media='all'"
+        />
+        <noscript>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Asta+Sans:wght@100;200;300;700&family=Cascadia+Code:wght@200;300;500;700&display=swap"
+            rel="stylesheet"
+          />
+        </noscript>
+      </head>
+      <body className={`${orbit.variable} ${sourceCodePro.variable}`}>
         <div className="max-w-3xl mx-auto px-4 py-8">
           {children}
           <Footer />
